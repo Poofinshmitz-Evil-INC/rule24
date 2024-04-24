3d9d3c94329c45f66a967a8d258cac5d63752b71
Tejo-TV <150041189+Tejo-TV@users.noreply.github.com>
2024-04-24 09:10:34 +0200

    Index biginning

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//---------------------------------------------------------------------------------------------------//
+// Naam script		: index.php
+// Omschrijving		: De hoofdpagina van de website
+// Naam ontwikkelaar: Tejo Veldman
+// Project		    : hackethon
+// Datum		    : 24-4-2024
+//---------------------------------------------------------------------------------------------------//
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    
+</body>
+</html>

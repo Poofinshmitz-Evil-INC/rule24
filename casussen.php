cbfdee4d54606e15de20256501469dd904da2ba3
Mohamed Jelani <144230887+NUMMLOS@users.noreply.github.com>
2024-04-24 13:51:42 +0200

    changes
    delete header&footer.css changes in casus.css background color  back to white and header changes
    are logo image and login image plus create nav and made all in grid

--- casussen.php
+++ casussen.php
@@ -9,19 +9,13 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Casussen</title>
-    <link rel="stylesheet" href="style folder/casus.css">
-</head>
-<body>
+
 <?php include("header and footer/header.php"); ?>
 <section>
 <article class="product">
     <h1>product</h1>
     <p>
-        <le></le>
+        
     </p>
 </article>
 <article class="product">
--- header and footer/header.php
+++ header and footer/header.php
@@ -1,6 +1,11 @@
-<link rel="stylesheet" href="style folder/header&footer.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Casussen</title>
+    <link rel="stylesheet" href="style folder/casus.css">
+</head>
+<body>
 <section class="headersection">
-    
 <img class="Logo" src="" alt="logo">
 <article class="container">
 <ul id="navigation">
@@ -10,4 +15,5 @@
         </ul>
 </article>
 
+<a class="login-image" href=""><img class="login-image" src="images folder/login.png" alt=""></a>
 </section>

99e817476124c9630f113709f575cfd17d88ec1a
Tejo-TV <150041189+Tejo-TV@users.noreply.github.com>
2024-04-25 14:03:04 +0200

    update img inprove

--- homepage.php
+++ homepage.php
@@ -1,22 +1,5 @@
 <?php
 //---------------------------------------------------------------------------------------------------//
-// Naam script		: index.php
-// Omschrijving		: De hoofdpagina van de website
-// Naam ontwikkelaar: Tejo Veldman
-// Project		    : hackethon
-// Datum		    : 24-4-2024
-//---------------------------------------------------------------------------------------------------//
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="casus.css">
-    <title>index</title>
-</head>
-<?php
-//---------------------------------------------------------------------------------------------------//
 // Naam script      : index.php
 // Omschrijving     : De hoofdpagina van de website
 // Naam ontwikkelaar: Tejo Veldman
@@ -36,11 +19,11 @@
  
 <div class="container1">
 <div class="left-image">
-<img src="images/team.jpg" alt="Linker afbeelding">
+<img src="images/team2.png" alt="Linker afbeelding">
 </div>
 <div class="right-content">
 <img src="images/team.jpg" alt="Rechter afbeelding">
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+<p>Welkom op onze website gewijd aan het innovatieve platform voor educatieve projecten in Zoetermeer en de Community Dutch Innovation Factory. Hier vindt u een overzicht van educatieve projecten voor leerlingen en studenten, verzameld en gepresenteerd in samenwerking met de gemeente Zoetermeer. Met gemakkelijke selectieopties en een geïntegreerd volgsysteem streven we ernaar om een handige en efficiënte ervaring te bieden voor zowel projectaanbieders als -zoekenden. Ontdek ons aanbod en sluit u aan bij onze gemeenschap die innovatie en educatie hoog in het vaandel draagt.</p>
 </div>
 </div>
  
--- partnerpage.php
+++ partnerpage.php
@@ -10,13 +10,13 @@
 <?php include("header&footer/header.php"); ?>
     <main class="partners">
         <section>
-            <article>
+            <article class="partnertext">
                 <h1>
-                    partneren met ons
+                    Partneren met ons
                 </h1>
                 <p>
                     Ben jij een bedrijf dat uitblinkt in het vinden van innovatieve oplossingen voor uitdagende situaties? Wij nodigen je uit om onze partner te worden en jouw expertise te delen door middel van casussen op ons platform. Als partner krijg je de kans om jouw unieke aanpak en strategieën te presenteren aan een breder publiek. Door jouw casussen te delen, help je anderen in jouw branche te leren van jouw ervaringen en successen. Word vandaag nog onze partner en draag bij aan het delen van waardevolle kennis en expertise binnen onze gemeenschap van professionals!
-            </p>
+                </p>
             </article>
             <article>
                 <img class="UI" src="images/business.png">

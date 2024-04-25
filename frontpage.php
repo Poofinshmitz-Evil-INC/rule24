3a41425b7d48227962de28abe5c91e756b93ecbb
Tejo-TV <150041189+Tejo-TV@users.noreply.github.com>
2024-04-25 09:37:41 +0200

    frontpage edit

---
+++ frontpage.php
@@ -0,0 +1,26 @@
+<?php
+//---------------------------------------------------------------------------------------------------//
+// Naam script		: index.php
+// Omschrijving		: De hoofdpagina van de website
+// Naam ontwikkelaar: Tejo Veldman
+// Project		    : hackethon
+// Datum		    : 24-4-2024
+//---------------------------------------------------------------------------------------------------//
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="casus.css">
+    <title>index</title>
+</head>
+<?php include("header&footer/header.php"); ?>
+<body>
+    <div class="textHomepage">
+        <p>Welkom op onze website gewijd aan het innovatieve platform voor educatieve projecten in Zoetermeer en de Community Dutch Innovation Factory. Hier vindt u een overzicht van educatieve projecten voor leerlingen en studenten, verzameld en gepresenteerd in samenwerking met de gemeente Zoetermeer. Met gemakkelijke selectieopties en een geïntegreerd volgsysteem streven we ernaar om een handige en efficiënte ervaring te bieden voor zowel projectaanbieders als -zoekenden. Ontdek ons aanbod en sluit u aan bij onze gemeenschap die innovatie en educatie hoog in het vaandel draagt.</p>
+    </div>
+    <img src="images/team.jpg" alt="team photo" class="team">
+</body>
+<?php include("header&footer/footer.php"); ?>
+</html>
